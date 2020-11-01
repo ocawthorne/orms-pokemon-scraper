3d653df6913201fd7cb7f79b371ed7269c7d67f0
ocawthorne <ocawthorne@gmail.com>
2020-11-01 20:00:06 +0000

    orms-pokemon-011120

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+    attr_reader :id
+    attr_accessor :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id, @name, @type, @db = id, name, type, db
+      end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+      def self.find(id_num, db)
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num)[0]
+        self.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+      end
+    
 end
